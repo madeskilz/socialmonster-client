04c30c2cc72f9021401ca5e78ae5bd8e68735eee
madeskilz <michaeladekoniye@gmail.com>
2019-10-28 17:02:36 +0100

    Updates

---
+++ precache-manifest.1a2e680ca9278e0d37629f063c8be507.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68334eec4683c4e986bac7e6aa9929f1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aa675e86f7d2e4e0b654",
+    "url": "/static/css/main.f78bb284.chunk.css"
+  },
+  {
+    "revision": "24263a28b56f91b552b2",
+    "url": "/static/js/2.8f52594f.chunk.js"
+  },
+  {
+    "revision": "aa675e86f7d2e4e0b654",
+    "url": "/static/js/main.49328a4b.chunk.js"
+  },
+  {
+    "revision": "d3c4e84721489bb3cf85",
+    "url": "/static/js/runtime-main.65ff4515.js"
+  },
+  {
+    "revision": "54c93b83c1005f4e6525847dd9ac3be2",
+    "url": "/static/media/logo.54c93b83.jpg"
+  },
+  {
+    "revision": "f473a349e8ebf395d007a9356a42a5e0",
+    "url": "/static/media/no-img.f473a349.png"
+  }
+]);
